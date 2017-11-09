138cd5773e707399b59260297349c41b229ce674
MikeSolvalou <godinemich@myvuw.ac.nz>
2017-11-10 11:00:22 +1300

    Replaced HBox with Halignment setting.

--- GUIpractice/src/login/Login.java
+++ GUIpractice/src/login/Login.java
@@ -2,6 +2,7 @@ package login;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -29,35 +29,33 @@ public class Login extends Application {
 		Text scenetitle = new Text("Welcome");
 		scenetitle.setId("welcome-text");
 		grid.add(scenetitle, 0, 0, 2, 1);
-
+		
 		Label userName = new Label("User Name:");
 		grid.add(userName, 0, 1);
-
+		
 		TextField userTextField = new TextField();
 		grid.add(userTextField, 1, 1);
-
+		
 		Label pw = new Label("Password:");
 		grid.add(pw, 0, 2);
-
+		
 		PasswordField pwBox = new PasswordField();
 		grid.add(pwBox, 1, 2);
 		
-		//grid.setGridLinesVisible(true);
+		grid.setGridLinesVisible(true);
 		
 		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
+		grid.add(btn, 1, 4);
+		GridPane.setHalignment(btn, HPos.RIGHT);
 		
 		final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-        actiontarget.setId("actiontarget");
-        
-        btn.setOnAction( (ActionEvent event) -> {
-            actiontarget.setText("Sign in button pressed");
-        });
-        
+		grid.add(actiontarget, 1, 6);
+		actiontarget.setId("actiontarget");
+		
+		btn.setOnAction( (ActionEvent event) -> {
+			actiontarget.setText("Sign in button pressed");
+		});
+		
 		
 		Scene scene = new Scene(grid, 300, 275);
 		scene.getStylesheets().add(Login.class.getResource("login.css").toExternalForm());
